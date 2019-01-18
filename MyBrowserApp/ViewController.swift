bb95c742ee3a7d0d3fcc993ce3373fe928d103ba
Terashima <yamanogomora@gmail.com>
2019-01-18 19:31:02 +0900

    #04 部品を接続していこう

--- MyBrowserApp/ViewController.swift
+++ MyBrowserApp/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var browserWebView: UIWebView!
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goBack(_ sender: Any) {
+    }
+    
+    @IBAction func goForward(_ sender: Any) {
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+    }
 }
 
